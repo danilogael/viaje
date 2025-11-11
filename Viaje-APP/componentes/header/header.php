0f681f3703a1d7da97e9435bcefe47366f58ac2f
danilogael <danilogaelmm@gmail.com>
2025-11-10 20:30:49 -0600

    header y footer

---
+++ Viaje-APP/componentes/header/header.php
@@ -0,0 +1,21 @@
+<header>
+  <div class="logo-titulo">
+    <img src="/viaje/viaje/Viaje-APP/componentes/imagenes/Logo.png" alt="Logo Remolinos Tours" width="100">
+    <h1>Remolinos Tours</h1>
+  </div>
+
+  <nav>
+    <ul class="nav-links" id="nav-links">
+      <li><a href="/viaje/viaje/Viaje-APP/default.php">Listo para tu viaje</a></li>
+      <li><a href="/viaje/viaje/Viaje-APP/componentes/paquetes/paquete.php">Paquetes</a></li>
+      <li><a href="/viaje/viaje/Viaje-APP/componentes/Planea/planea.php">Planea tu viaje</a></li>
+      <li><a href="/viaje/viaje/Viaje-APP/componentes/ofertas/ofertas.php">Ofertas</a></li>
+      <li><a href="/viaje/viaje/Viaje-APP/componentes/iniciarsesion/sign.php">Iniciar sesión</a></li>
+      <li><a href="#">Idioma y Moneda</a></li>
+    </ul>
+  </nav>
+
+  <button id="menu-toggle" class="menu-toggle" aria-label="Abrir menú">☰</button>
+</header>
+
+  
